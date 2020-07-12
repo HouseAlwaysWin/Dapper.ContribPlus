7e9af91366d6059e0434b04d08478d531e62a8ad
MartinWang <martinwang7963@gmail.com>
2020-07-12 19:30:19 +0800

    fix test

--- Dapper.ContribPlus.Tests/CRUDTests.cs
+++ Dapper.ContribPlus.Tests/CRUDTests.cs
@@ -129,12 +129,12 @@ namespace Dapper.ContribPlus.Tests
             using (var conn = new SqlConnection(connectionString))
             {
                 var result = conn.GetListByPaging<TestOrderBy>(1, 10);
-                Assert.AreEqual("a", result.data.ToList()[0].Name);
-                Assert.AreEqual("a", result.data.ToList()[1].Name);
+                Assert.AreEqual("n", result.data.ToList()[0].Name);
+                Assert.AreEqual("o", result.data.ToList()[1].Name);
 
                 result = conn.GetListByPaging<TestOrderBy>(2, 6);
-                Assert.AreEqual("b", result.data.ToList()[0].Name);
-                Assert.AreEqual("c", result.data.ToList()[5].Name);
+                Assert.AreEqual("a", result.data.ToList()[0].Name);
+                Assert.AreEqual("a", result.data.ToList()[5].Name);
 
                 conn.Execute("DROP TABLE [dbo].[Test]");
                 Assert.Pass();
